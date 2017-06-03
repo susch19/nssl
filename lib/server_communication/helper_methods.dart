119951176054d27349fde68f020b6943b5e3b0a5
susch19 <suschi@ist-einmalig.de>
2017-06-03 17:00:29 +0200

    Implemented refresh of token once per day, when the app is used

--- lib/server_communication/helper_methods.dart
+++ lib/server_communication/helper_methods.dart
@@ -2,38 +2,53 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:async';
+import 'package:testProject/manager/file_manager.dart';
 import 'package:testProject/models/model_export.dart';
+import 'package:testProject/server_communication/jwt.dart';
+import 'user_sync.dart';
 
 class HelperMethods {
   static const String url = "https://susch.undo.it:443";
 
   static Future<http.Response> post(String path, [Object body = null]) async {
-    var g = http.post("$url/$path",
-        body: JSON.encode(body), headers: {"Content-Type": "application/json", User.token==null?"X-foo":"X-Token":User.token});
+    await handleTokenRefresh();
+    var g = http.post("$url/$path", body: JSON.encode(body), headers: {
+      "Content-Type": "application/json",
+      User.token == null ? "X-foo" : "X-Token": User.token
+    });
     http.Response res;
     await g.then((x) => res = x);
     return res;
   }
 
   static Future<http.Response> get(String path) async {
-    var g = http.get("$url/$path",
-        headers: {"Content-Type": "application/json", User.token==null?"X-foo":"X-Token":User.token});
+    await handleTokenRefresh();
+    var g = http.get("$url/$path", headers: {
+      "Content-Type": "application/json",
+      User.token == null ? "X-foo" : "X-Token": User.token
+    });
     http.Response res;
     await g.then((x) => res = x);
     return res;
   }
 
   static Future<http.Response> put(String path, [Object body = null]) async {
-    var g = http.put("$url/$path",
-        body: JSON.encode(body), headers: {"Content-Type": "application/json", User.token==null?"X-foo":"X-Token":User.token});
+    await handleTokenRefresh();
+    var g = http.put("$url/$path", body: JSON.encode(body), headers: {
+      "Content-Type": "application/json",
+      User.token == null ? "X-foo" : "X-Token": User.token
+    });
     http.Response res;
     await g.then((x) => res = x);
     return res;
   }
 
   static Future<http.Response> delete(String path) async {
-    var g = http.delete("$url/$path",
-        headers: {"Content-Type": "application/json", User.token==null?"X-foo":"X-Token":User.token});
+    await handleTokenRefresh();
+    var g = http.delete("$url/$path", headers: {
+      "Content-Type": "application/json",
+      User.token == null ? "X-foo" : "X-Token": User.token
+    });
     http.Response res;
     await g.then((x) => res = x);
     return res;
@@ -44,12 +59,24 @@ class HelperMethods {
     print(jsonString);
   }
 
-  static bool reactToRespone(http.Response respone, {BuildContext context, ScaffoldState scaffoldState}){
-    if(respone.statusCode != 200)
-    {
-      scaffoldState.showSnackBar(new SnackBar(content: new Text(respone.reasonPhrase)));
+  static bool reactToRespone(http.Response respone,
+      {BuildContext context, ScaffoldState scaffoldState}) {
+    if (respone.statusCode != 200) {
+      scaffoldState
+          .showSnackBar(new SnackBar(content: new Text(respone.reasonPhrase)));
       return false;
     }
     return true;
+  }
+
+  static handleTokenRefresh() async {
+    if (await JWT.newToken()) {
+      var t = await UserSync.refreshToken();
+      var m = JSON.decode(t.body);
+      var to = m["token"];
+      FileManager.write("token.txt", to);
+      User.token = to;
+
+    }
   }
 }
---
+++ lib/server_communication/jwt.dart
@@ -0,0 +1,14 @@
+import 'dart:async';
+import 'package:jwt/json_web_token.dart';
+import 'package:testProject/models/user.dart';
+
+class JWT {
+  static Future<bool> newToken() async {
+    JsonWebTokenDecoder dec = new JsonWebTokenDecoder();
+    var map = dec.convert(User.token);
+    DateTime expires = DateTime.parse(map["expires"].substring(0, 10));
+    if ((new DateTime.now()).add(new Duration(days: 29)).isAfter(expires))
+      return true;
+    return false;
+  }
+}
