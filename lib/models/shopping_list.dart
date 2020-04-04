fb7b5cbfc199e5c81d262f31bf58be34c1273078
Daniel Henk <henk.daniel98@gmail.com>
2020-04-04 02:25:39 +0200

    Adds ordering to shoppinglist and mainview

--- lib/models/shopping_list.dart
+++ lib/models/shopping_list.dart
@@ -15,25 +15,39 @@ class ShoppingList {
 
   Future save() async {
     await DatabaseManager.database.transaction((z) async {
-      int count =
-          await z.rawUpdate('UPDATE ShoppingLists SET name = ?, messaging = ? WHERE id = ?', [name, messagingEnabled ? 1 : 0, id]);
+      int count = await z.rawUpdate('UPDATE ShoppingLists SET name = ?, messaging = ? WHERE id = ?', [name, messagingEnabled ? 1 : 0, id]);
       if (count == 0)
-        await z
-            .rawInsert('INSERT INTO ShoppingLists(id, name, messaging, user_id) VALUES(?, ?, ?, ?)', [id, name, messagingEnabled ? 1 : 0, User.ownId]);
+        await z.rawInsert('INSERT INTO ShoppingLists(id, name, messaging, user_id) VALUES(?, ?, ?, ?)', [id, name, messagingEnabled ? 1 : 0, User.ownId]);
 
       await z.rawDelete("DELETE FROM ShoppingItems WHERE res_list_id = ?", [id]);
       for (var item in shoppingItems) {
-        await z.rawInsert("INSERT INTO ShoppingItems(id, name, amount, crossed, res_list_id) VALUES (?, ?, ?, ?, ?)",
-            [item.id, item.name, item.amount, item.crossedOut ? 1 : 0, id]);
+        await z.execute("INSERT OR REPLACE INTO ShoppingItems(id, name, amount, crossed, res_list_id, sortorder) VALUES (?, ?, ?, ?, ?, ?)",
+            [item.id, item.name, item.amount, item.crossedOut ? 1 : 0, id, item.sortOrder]);
       }
     });
-    
   }
 
   static Future<List<ShoppingList>> load() async {
     var lists = await DatabaseManager.database.rawQuery("SELECT * FROM ShoppingLists WHERE user_id = ?", [User.ownId]);
 
-    var items = await DatabaseManager.database.rawQuery("SELECT * FROM ShoppingItems");
+    var items = await DatabaseManager.database.rawQuery("SELECT * FROM ShoppingItems ORDER BY res_list_id, sortorder");
+
+    // TODO: if db ordering enough for us, or do we want to order by ourself in code?
+    // return lists
+    //     .map((x) => new ShoppingList()
+    //       ..id = x["id"]
+    //       ..messagingEnabled = x["messaging"] == 0 ? false : true
+    //       ..name = x["name"]
+    //       ..shoppingItems = (items
+    //           .where((y) => y["res_list_id"] == x["id"])
+    //           .map((y) => new ShoppingItem(y["name"])
+    //             ..amount = y["amount"]
+    //             ..crossedOut = y["crossed"] == 0 ? false : true
+    //             ..id = y["id"]
+    //             ..sortOrder = y["sortorder"])
+    //           .toList()
+    //             ..sort((a, b) => a.sortOrder?.compareTo(b.sortOrder))))
+    //     .toList();
 
     return lists
         .map((x) => new ShoppingList()
@@ -45,7 +59,8 @@ class ShoppingList {
               .map((y) => new ShoppingItem(y["name"])
                 ..amount = y["amount"]
                 ..crossedOut = y["crossed"] == 0 ? false : true
-                ..id = y["id"])
+                ..id = y["id"]
+                ..sortOrder = y["sortorder"])
               .toList())
         .toList();
   }
@@ -61,7 +76,7 @@ class ShoppingList {
 //      return;
 //    }
     var newList = GetListResult.fromJson(res.body);
-    var crossedOut = (await DatabaseManager.database.rawQuery("SELECT id, crossed FROM ShoppingItems WHERE res_list_id = ?", [id]));
+    var items = (await DatabaseManager.database.rawQuery("SELECT id, crossed, sortorder FROM ShoppingItems WHERE res_list_id = ?", [id]));
 
     shoppingItems.clear();
     for (var item in newList.products)
@@ -70,8 +85,10 @@ class ShoppingList {
         ..amount = item.amount
         ..changed = item.changed
         ..created = item.created
-        ..crossedOut = (crossedOut.firstWhere((x) => x["id"] == item.id, orElse: () => {"crossed": 0})["crossed"] == 0 ? false : true))
-        ;
+        ..crossedOut = (items.firstWhere((x) => x["id"] == item.id, orElse: () => {"crossed": 0})["crossed"] == 0 ? false : true)
+        ..sortOrder = (items.firstWhere((x) => x["id"] == item.id, orElse: () => {"sortorder": 0})["sortorder"]));
+
+    shoppingItems.sort((a, b) => a.sortOrder.compareTo(b.sortOrder));
     save();
   }
 
@@ -80,7 +97,7 @@ class ShoppingList {
     User.shoppingLists.clear();
     await DatabaseManager.database.delete("ShoppingLists", where: "user_id = ?", whereArgs: [User.ownId]);
     //await DatabaseManager.database.rawDelete("DELETE FROM ShoppingLists where user_id = ?", [User.ownId]);
-    var crossedOut = (await DatabaseManager.database.rawQuery("SELECT id, crossed FROM ShoppingItems WHERE crossed = 1"));
+    var items = (await DatabaseManager.database.rawQuery("SELECT id, crossed, sortorder FROM ShoppingItems"));
     for (var res in result.shoppingLists) {
       var list = new ShoppingList()
         ..id = res.id
@@ -91,7 +108,10 @@ class ShoppingList {
         list.shoppingItems.add(new ShoppingItem(item.name)
           ..id = item.id
           ..amount = item.amount
-          ..crossedOut = (crossedOut.firstWhere((x) => x["id"] == item.id, orElse: () => {"crossed": 0})["crossed"] == 0 ? false : true));
+          ..crossedOut = (items.firstWhere((x) => x["id"] == item.id, orElse: () => {"crossed": 0})["crossed"] == 0 ? false : true)
+          ..sortOrder = (items.firstWhere((x) => x["id"] == item.id, orElse: () => {"sortorder": 0})["sortorder"]));
+
+      list.shoppingItems.sort((a, b) => a.sortOrder.compareTo(b.sortOrder));
       User.shoppingLists.add(list);
 
       list.subscribeForFirebaseMessaging();
@@ -102,6 +122,4 @@ class ShoppingList {
   void subscribeForFirebaseMessaging() => firebaseMessaging.subscribeToTopic(id.toString() + "shoppingListTopic");
 
   void unsubscribeFromFirebaseMessaging() => firebaseMessaging.unsubscribeFromTopic(id.toString() + "shoppingListTopic");
-
-
 }
